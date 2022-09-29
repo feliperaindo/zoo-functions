ede9720042ac45717e0fa9fbeea6c8cbe908defd
feliperaindo <felipe.raindo@gmail.com>
2022-09-29 16:26:31 -0300

    inicia requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) {
+    return [];
+  }
+  return a;
 }
 
 module.exports = getSpeciesByIds;
