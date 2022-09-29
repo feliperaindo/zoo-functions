089bbd548970600ba843f65b96af154663be0bb2
feliperaindo <felipe.raindo@gmail.com>
2022-09-29 18:04:12 -0300

    resolve requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -4,7 +4,16 @@ function getSpeciesByIds(...ids) {
   if (ids.length === 0) {
     return [];
   }
-  return a;
+  const getAnimals = [];
+
+  ids.forEach((id) => {
+    const checkId = data.species.find((animal) => animal.id === id);
+    if (checkId !== undefined) {
+      getAnimals.push(checkId.name);
+    }
+  });
+
+  return getAnimals;
 }
 
 module.exports = getSpeciesByIds;
