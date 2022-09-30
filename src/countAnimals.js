99e88e2a468000ad61ce6faf3942383dfe7a4fa1
feliperaindo <felipe.raindo@gmail.com>
2022-09-30 14:51:59 -0300

    update

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals(animal = data.species) {
+  const animalsObj = {};
+
+  animal.forEach((eachAnimal) => {
+    const { name, residents } = eachAnimal;
+    animalsObj[name] = residents.length;
+  });
+
+  return animalsObj;
 }
 
 module.exports = countAnimals;
