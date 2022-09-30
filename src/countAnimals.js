79f116b8685de53cd5e8c36a00d6ac3dd35c26d6
feliperaindo <felipe.raindo@gmail.com>
2022-09-30 17:26:06 -0300

    resolve requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,14 +1,43 @@
+// const ageFiltered = filterSexAndAge(specieFiltered, 'age', age);
+// const sexFiltered = filterSexAndAge(ageFiltered, 'sex', sex);
+
+// return animal.reduce((animalsObj, eachObj) => {
+//   animalsObj[eachObj.name] = eachObj.residents.length;
+//   return animalsObj;
+// }, {});
+// countAnimals({ specie: 'bears' });
+
 const data = require('../data/zoo_data');
 
-function countAnimals(animal = data.species) {
+function undefinedParam() {
   const animalsObj = {};
-
-  animal.forEach((eachAnimal) => {
-    const { name, residents } = eachAnimal;
-    animalsObj[name] = residents.length;
-  });
-
+  data.species.forEach((eachObj) => { animalsObj[eachObj.name] = eachObj.residents.length; });
   return animalsObj;
+}
+
+function undefinedSex(specie) {
+  const getSpecieObj = data.species.find((animalObj) => animalObj.name === specie);
+  return JSON.stringify(getSpecieObj);
+}
+
+function specieAndSexDefined(specie, sex) {
+  const getFilteredSpecie = JSON.parse(undefinedSex(specie));
+  const getFilteredSex = getFilteredSpecie.residents.filter((condition) => condition.sex === sex);
+  return getFilteredSex.length;
+}
+
+function countAnimals(animal) {
+  if (animal === undefined) {
+    return undefinedParam();
+  }
+
+  const { specie, sex } = animal;
+
+  if (sex === undefined) {
+    const objReturned = JSON.parse(undefinedSex(specie));
+    return objReturned.residents.length;
+  }
+  return specieAndSexDefined(specie, sex);
 }
 
 module.exports = countAnimals;
