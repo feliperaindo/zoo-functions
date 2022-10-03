562da03f19ae0824d2ad857e4607c2907a09a890
Felipe <felipe.raindo@gmail.com>
2022-10-03 04:51:23 -0300

    resolve questão 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,55 @@
 const data = require('../data/zoo_data');
 
+const { hours } = data;
+const weekDays = Object.keys(hours);
+
+const checkAnimal = (parameter) => data.species.find((animal) => animal.name === parameter);
+
+const arrayConstructorAnimals = (weekDay) => {
+  const animals = data.species.reduce((animaisArray, eachAnimal) => {
+    if (eachAnimal.availability.includes(weekDay)) {
+      animaisArray.push(eachAnimal.name);
+    }
+    return animaisArray;
+  }, []);
+  return animals;
+};
+
+function tableSchedule(filterDays) {
+  const table = {};
+  filterDays.forEach((weekDay) => {
+    const { open, close } = hours[weekDay];
+    table[weekDay] = {};
+    if (weekDay === 'Monday') {
+      table[weekDay].officeHour = 'CLOSED';
+      table[weekDay].exhibition = 'The zoo will be closed!';
+    } else {
+      table[weekDay].officeHour = `Open from ${open}am until ${close}pm`;
+      table[weekDay].exhibition = arrayConstructorAnimals(weekDay);
+    }
+  });
+  return table;
+}
+
+function filterDayOrAnimal(string, animalResult, dayResult) {
+  if (dayResult) {
+    return tableSchedule([string]);
+  }
+  return animalResult.availability;
+}
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (scheduleTarget === undefined) {
+    return tableSchedule(weekDays);
+  }
+
+  const animal = checkAnimal(scheduleTarget);
+  const day = weekDays.includes(scheduleTarget);
+
+  if (animal === undefined && day === false) {
+    return tableSchedule(weekDays);
+  }
+  return filterDayOrAnimal(scheduleTarget, animal, day);
 }
 
 module.exports = getSchedule;
