1dce539615815040dd1ba65c3896f14dc3032f5b
Felipe <felipe.raindo@gmail.com>
2022-10-04 15:52:42 -0300

    quase lá

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,49 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const arrayCreator = (filter) => filter.reduce((array, animal) => [...array, animal.name], []);
+
+const filterLocation = (location) => species.filter((animal) => animal.location === location);
+
+const insertAnimalsInArray = (object, array) => {
+  array.forEach((eachAnimalObject) => {
+    const { residents, location, name } = eachAnimalObject;
+    const newObj = { [name]: arrayCreator(residents) };
+    object[location].push(newObj);
+  });
+  return object;
+};
+
+const includeNamesDefined = () => {
+  const dataNames = { NE: [], NW: [], SE: [], SW: [] };
+  Object.keys(dataNames).forEach((location) => {
+    const animalsInLocation = filterLocation(location);
+    insertAnimalsInArray(dataNames, animalsInLocation);
+  });
+  return dataNames;
+};
+
+const includeNamesUndefined = () => {
+  const dataNames = { NE: [], NW: [], SE: [], SW: [] };
+  Object.keys(dataNames).forEach((location) => {
+    const animalsInLocation = filterLocation(location);
+    const arrayAnimalsZoo = arrayCreator(animalsInLocation);
+    dataNames[location] = [...arrayAnimalsZoo];
+  });
+  return dataNames;
+};
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (!options) {
+    return includeNamesUndefined();
+  }
+  const { includeNames } = options;
+
+  if (!includeNames) {
+    return includeNamesUndefined();
+  }
+  return includeNamesDefined();
 }
 
 module.exports = getAnimalMap;
